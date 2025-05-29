50e30c4e847c88a60d90e684336d7e00648cdfe1
Md Shah Aman Patwary <taijulaman8@gmail.com>
2025-05-29 12:01:05 +0600

    gsap tween using html,css,js

--- index.js
+++ index.js
@@ -4,7 +4,11 @@
 gsap.to(".green", { rotation: 360, x: 500, duration: 5 });
 
 // target the element with a class of "purple" - rotate and move FROM 100px to the left over the course of 1 second.
-gsap.from(".purple", { rotation: -360, x: -100, duration: 1 });
+gsap.from(".purple", { rotation: 360, x: 500, duration: 10 });
 
 // target the element with a class of "blue" - rotate and move FROM 100px to the left, TO 100px to the right over the course of 1 second.
-gsap.fromTo(".blue", { x: -100 }, { rotation: 360, x: 100, duration: 1 });
+gsap.fromTo(
+  ".blue",
+  { x: 100, rotation: -360, duration: 5 },
+  { x: 500, rotation: 360, duration: 5 }
+);
